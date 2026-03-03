72473134a182cb0dad4df0b1ed03e663c532da9e
omar kamel <isomardev@gmail.com>
2026-03-03 22:05:12 +0200

    chore: migrate jest config to ESM with ts-jest preset

--- characterization-testing/jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  transform: {
-    '^.+\\.ts?$': 'ts-jest',
-  },
-  testEnvironment: 'node',
-  testRegex: './tests/.*\\.(test|spec)?\\.(ts|ts)$',
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-  roots: ['<rootDir>/tests'],
-};
---
+++ characterization-testing/jest.config.ts
@@ -0,0 +1,13 @@
+import type { Config } from 'jest';
+
+const config: Config = {
+  preset: 'ts-jest/presets/default-esm',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/tests'],
+  testMatch: ['**/*.test.ts', '**/*.spec.ts'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  clearMocks: true,
+  setupFilesAfterEnv: ['<rootDir>/tests/setup.ts'],
+};
+
+export default config;
